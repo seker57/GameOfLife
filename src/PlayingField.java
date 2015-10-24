8c110bfed902bbf01052159a1f8e21fbb2ee40db
tademeit <timo.ademeit@gmx.de>
2015-10-24 11:32:18 +0200

    U PlayingField.java; Added Getter for fieldcells

--- src/PlayingField.java
+++ src/PlayingField.java
@@ -4,9 +4,22 @@
 public class PlayingField {
 
     private static int length, width;
+    private static char[] fieldcells;
 
     public PlayingField(int length, int width){
         this.length = length;
         this.width = width;
+
+        fieldcells = new char[(length*width) - 1];
+    }
+
+    public void printFieldStatus(){
+
+
+
+    }
+
+    public char[] getFieldCells(){
+        return this.fieldcells;
     }
 }
